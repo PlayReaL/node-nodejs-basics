2c31681f19718d1b152a1118cc7754955a67e277
PlayReaL <PlayReaL@users.noreply.github.com>
2024-01-23 21:39:03 +0000

    feat: implement fs/copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,21 @@
+import * as fs from "node:fs";
+import * as path from "node:path";
+import { fileURLToPath } from "url";
+
 const copy = async () => {
-    // Write your code here 
+    const fileName = fileURLToPath(import.meta.url);
+    const srcPath = path.join(path.dirname(fileName), "files");
+    const destPath = path.join(path.dirname(fileName), "files_copy");
+
+    try {
+        await fs.promises.cp(srcPath, destPath, {
+            errorOnExist: true,
+            force: false,
+            recursive: true,
+        });
+    } catch (error) {
+        throw new Error("FS operation failed");
+    }
 };
 
 await copy();
