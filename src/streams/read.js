f00c15973af26caf8b91145522c1b962bac84c79
PlayReaL <PlayReaL@users.noreply.github.com>
2024-01-25 17:43:21 +0000

    feat: implement streams/read

--- src/streams/read.js
+++ src/streams/read.js
@@ -3,7 +3,15 @@ import * as path from "node:path";
 import { fileURLToPath } from "url";
 
 const read = async () => {
+    const fileName = fileURLToPath(import.meta.url);
+    const filePath = path.join(
+        path.dirname(fileName),
+        "files",
+        "fileToRead.txt"
+    );
 
+    const stream = fs.createReadStream(filePath);
+    stream.on("data", (chunk) => process.stdout.write(chunk));
 };
 
 await read();
