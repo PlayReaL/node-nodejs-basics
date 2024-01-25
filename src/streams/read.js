2fab76caff84fe31cb07cefbe3b2a4985eadbdeb
PlayReaL <PlayReaL@users.noreply.github.com>
2024-01-25 17:38:41 +0000

    feat: implement hash/calcHash

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,9 @@
+import * as fs from "node:fs";
+import * as path from "node:path";
+import { fileURLToPath } from "url";
+
 const read = async () => {
-    // Write your code here 
+
 };
 
 await read();
