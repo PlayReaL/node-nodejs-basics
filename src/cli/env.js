dd923d90b23d4369a7fa13c5510f24bdea45c9b0
PlayReaL <PlayReaL@users.noreply.github.com>
2024-01-25 12:13:00 +0000

    feat: implement cli/env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
 const parseEnv = () => {
-    // Write your code here 
+    let res = "";
+
+    for (let k in process.env) {
+        if (k.slice(0, 4) === "RSS_") {
+            res += `${k}=${process.env[k]}; `;
+        }
+    }
+    console.log(res.slice(0, -2));
 };
 
 parseEnv();
