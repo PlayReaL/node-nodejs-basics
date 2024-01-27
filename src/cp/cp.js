73ff8e14be7db2014d6fedbedc103687ca44121e
PlayReaL <PlayReaL@users.noreply.github.com>
2024-01-27 09:48:20 +0000

    feat: implement cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,12 @@
+import * as path from "path";
+import * as child_process from "child_process";
+import { fileURLToPath } from "url";
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const fileName = fileURLToPath(import.meta.url);
+    const dirPath = path.dirname(fileName);
+    child_process.fork(path.join(dirPath, "files", "script.js"), args);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["t1", "t2", 25]);
